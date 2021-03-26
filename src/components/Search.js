26ad18017a3b0b3f42f87e3c42f988044674bfab
timcastillogill <timgillviola@gmail.com>
2021-03-26 11:41:16 +0000

    Added search feature and return form for component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
 import axios from 'axios';
 
 export default class Search extends Component {
@@ -48,4 +54,72 @@ export default class Search extends Component {
       }
     })
   }
+
+  render() {
+    const searchForTutorials = this.state.results.filter((data) => {
+      if (this.state.searchQuery == null) {
+        return data;
+      } else if (data.videoTitle.toLwerCase().includes(this.state.searchQuery.toLowerCase()) || data.teacherName.toLowerCase().includes(this.state.searchQuery.toLowerCase())) {
+        return data;
+      }
+    }).map(data => {
+      return (
+        <Container maxWidth="sm">
+          <Card className="root">
+            <CardContent>
+              <Typography className="title">{data.videoTitle}</Typography>
+              <Typography className="url">{data.videoUrl}</Typography>
+              <Typography className="name">{data.teacherName}</Typography>
+            </CardContent>
+          </Card>
+        </Container>
+      )
+    })
+    return (
+      <div className="search">
+        <form>
+          <TextField
+            variant="filled"
+            className="inputSearch"
+            color="secondary"
+            type="text"
+            placeholder="Search for Tutorials..."
+            inputRef={input => this.serach = input}
+            onChange={this.handleSearchInputChange}
+            style={{
+              backgroundColor: '#5FBCDD',
+              margin: '10px',
+              width: '30%',
+              display: 'flex',
+              margin: 'auto',
+            }}
+            />
+          <Container maxWidth="sm">
+            {this.state.loadingData &&
+            <Button
+            className="button"
+            fullWidth="true"
+            variant="contained"
+            color="secondary"
+            type="button" >
+            Loading...</Button>}
+            {!this.state.loadingData &&
+            <Button
+              className="button"
+              fullWidth="true"
+              container=""
+              variant="contained"
+              color="secondary"
+              type="button" 
+              onClick={() => this.getInfo() }>
+            <span>Refresh Data</span>
+            </Button>}
+          </Container>
+        </form>
+        {/* {searchForTags} */}
+        {searchForTutorials}
+      </div>
+    )
+  }
+
 }
