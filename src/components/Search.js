55f6b7b08b93cbd8d6942fdc81696adc1727bf02
timcastillogill <timgillviola@gmail.com>
2021-03-26 11:18:19 +0000

    added get request for API ordered by userRating at source

--- src/components/Search.js
+++ src/components/Search.js
@@ -9,4 +9,15 @@ export default class Search extends Component {
     results: [],
     loadingData: false,
   }
+
+  getInfo = () => {
+    this.setState({ loadingData: true });
+    axios.get('https://lingumi-take-home-test-server.herokuapp.com/videoTutorials')
+    .then(({ data }) => {
+      data.sort((a, b) => b.averageUserRating - a.averageUserRating);
+        this.setState({ results: data });
+        this.setState({ loadingData: false });
+    })
+    .catch(() => this.setState({ error: true }));
+  }
 }
